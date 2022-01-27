07a0a066cf8b512715c2f91f8b44496bc26fe9f3
Gabriel Salles Braga <sallesbr@sheridancollege.ca>
2022-01-27 01:38:17 -0300

    Task 3: Completing CardTrick.java to model a hand of seven random cards

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -5,13 +5,15 @@
  */
 package ca.sheridancollege.week3.softwarefundamentals.ice1;
 
+
+
 /**
  * A class that fills a magic hand of 7 cards with random Card Objects
  * and then asks the user to pick a card and searches the array of cards
  * for the match to the user's card. To be used as starting code in ICE 1
  * @author dancye
- * @modified by Megha Patel
- * 
+ * @modified by Megha Patel and Gabriel Braga (Student ID: 991606560)
+ * @date 2022-01-25
  */
 public class CardTrick {
     
@@ -21,9 +23,17 @@ public class CardTrick {
         
         for (int i=0; i<magicHand.length; i++)
         {
-            Card c = new Card();
-             }
-        
-         }
+            magicHand[i] = new Card();
+            
+           // Setting randomly the suit and values
+            magicHand[i].setSuit(Card.SUITS[(int) (Math.random() * 4)]);
+            magicHand[i].setValue((int) (Math.random() * 13 + 1));
+            
+            System.out.println(magicHand[i].getSuit());
+            System.out.println(magicHand[i].getValue());
+            
+        }
+
+    }
     
 }
