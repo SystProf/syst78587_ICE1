e33983a97cda579ece8633410028d4df6971e881
Sathya Jeyachandran <jeyachsa@sheridancollege.ca>
2022-01-27 23:49:25 -0500

    My final change with Card and CardTrick

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/Card.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/Card.java
@@ -4,7 +4,8 @@
  * and open the template in the editor.
  */
 package ca.sheridancollege.week3.softwarefundamentals.ice1;
-
+import java.util.Random;
+import java.util.Scanner;
 /**
  * A class that models playing card Objects. Cards have 
  * a value (note that Ace = 1, Jack -11, Queen =12, King = 13)
@@ -22,10 +23,19 @@ public class Card {
    private String suit; //clubs, spades, diamonds, hearts
    private int value;//1-13
 
-   public static final String [] SUIT = {"Hearts", "Diamonds", "Spades", "Clubs"};
+   public static final String [] SUITS = {"Hearts", "Diamonds", "Spades", "Clubs"};
     /**
      * @return the suit
      */
+   
+   public Card(){
+       Random r=new Random();
+       int index=r.nextInt(SUITS.length);
+       int num=r.nextInt((13-1)+1)+1;
+        this.suit = SUITS[index];
+       this.value = num;
+   
+   }
     public String getSuit() {
         return suit;
     }
@@ -50,7 +60,15 @@ public class Card {
     public void setValue(int value) {
         this.value = value;
     }
-   
-   
+    
+   @Override
+    public String toString(){
+        return value + " of " + suit;
+    }
+    
+    public void printInfo(){
+        
+        System.out.println(value + " of " + suit + " was in the magic hands!");
+    }
     
 }
--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -4,7 +4,8 @@
  * and open the template in the editor.
  */
 package ca.sheridancollege.week3.softwarefundamentals.ice1;
-
+import java.util.Random;
+import java.util.Scanner;
 /**
  * A class that fills a magic hand of 7 cards with random Card Objects
  * and then asks the user to pick a card and searches the array of cards
@@ -18,15 +19,43 @@ public class CardTrick {
     
     public static void main(String[] args)
     {
+         Random r = new Random();
+        Scanner in = new Scanner(System.in);
         Card[] magicHand = new Card[7];
+        int count = 0;
         
-        for (int i=0; i<magicHand.length; i++)
-        {
-            Card c = new Card();
-            System.out.println(c.getSuit());
-             }
-        System.out.println("Hello");
-        System.out.println("Hello World");
-         }
+        // iterate through aray, for each spot in the array, create new card
+        for(int i = 0; i < magicHand.length; i++){
+            magicHand[i] = new Card();
+        }
+        
+        // iterate through array, print the card to console
+        for(int i = 0; i < magicHand.length; i++){
+            System.out.println(magicHand[i].toString());
+        }
+        
+        System.out.println("Enter a card in the given array Example:4,Spades");
+        // gets the user input
+        String userCardInput = in.next();
+        // split user input into array by the comma
+        String[] cardArray = userCardInput.split(",");
+        // convert from String to int
+        int number = Integer.parseInt(cardArray[0]);
+        String pickSuit = cardArray[1];
+        
+        
+        // iterate through the magicHand, determine if user input is same as one of the cards, then add 1
+        boolean result = true;
+        for(int i = 0; i < magicHand.length; i++){
+            if(magicHand[i].getValue() == number && magicHand[i].getSuit().equals(pickSuit)){
+                result= false;
+                magicHand[i].printInfo();
+            }
+
+        }
+        if(result){
+            System.out.println("Your input does not exist in the magic hands");
+        }
+    }
     
 }
