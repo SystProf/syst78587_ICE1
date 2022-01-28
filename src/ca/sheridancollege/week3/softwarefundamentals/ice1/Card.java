8615ca354cfeba1bc3815a49668b7a6b83f9dccf
suprun <suprun@sheridancollege.ca>
2022-01-27 23:58:21 -0500

    Completed all tasks up to and including 5

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/Card.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/Card.java
@@ -1,19 +1,15 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ca.sheridancollege.week3.softwarefundamentals.ice1;
 
 /**
  * A class that models playing card Objects. Cards have 
- * a value (note that Ace = 1, Jack -11, Queen =12, King = 13)
+ * a value (note that Ace = 1, Jack = 11, Queen = 12, King = 13)
  * A suit (clubs, hearts, spades, diamonds).
  * There are 52 cards in a deck, no jokers.
  * This code is to be used in ICE1. When you create your own branch,
  * add your name as a modifier.
  * @author dancye
  * @modified by Megha Patel
+ * @modified by Volodymyr Suprun 2022/01/25
  * 
  */
 public class Card {
@@ -49,7 +45,49 @@ public class Card {
     public void setValue(int value) {
         this.value = value;
     }
-   
-   
     
+    public void printInfo() {
+        //Switch statement used to print out what rank a card is depending on its value
+        switch (this.value) {
+            case 1:
+                System.out.println("Your card is the Ace of " + this.suit + " and it is in the magic hand!");
+                break;
+            case 2:
+                System.out.println("Your card is the Two of " + this.suit + " and it is in the magic hand!");
+                break;
+            case 3:
+                System.out.println("Your card is the Three of " + this.suit + " and it is in the magic hand!");
+                break;
+            case 4:
+                System.out.println("Your card is the Four of " + this.suit + " and it is in the magic hand!");
+                break;
+            case 5:
+                System.out.println("Your card is the Five of " + this.suit + " and it is in the magic hand!");
+                break;
+            case 6:
+                System.out.println("Your card is the Six of " + this.suit + " and it is in the magic hand!");
+                break;
+            case 7:
+                System.out.println("Your card is the Seven of " + this.suit + " and it is in the magic hand!");
+                break;
+            case 8:
+                System.out.println("Your card is the Eight of " + this.suit + " and it is in the magic hand!");
+                break;
+            case 9:
+                System.out.println("Your card is the Nine of " + this.suit + " and it is in the magic hand!");
+                break;
+            case 10:
+                System.out.println("Your card is the Ten of " + this.suit + " and it is in the magic hand!");
+                break;
+            case 11:
+                System.out.println("Your card is the Jack of " + this.suit + " and it is in the magic hand!");
+                break;
+            case 12:
+                System.out.println("Your card is the Queen of " + this.suit + " and it is in the magic hand!");
+                break;
+            case 13:
+                System.out.println("Your card is the King of " + this.suit + " and it is in the magic hand!");
+                break;
+        }
+    }
 }
--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -1,16 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ca.sheridancollege.week3.softwarefundamentals.ice1;
-
+import java.util.Scanner;
 /**
  * A class that fills a magic hand of 7 cards with random Card Objects
  * and then asks the user to pick a card and searches the array of cards
  * for the match to the user's card. To be used as starting code in ICE 1
  * @author dancye
  * @modified by Megha Patel
+ * @modified by Volodymyr Suprun 2022/01/25
  * 
  */
 public class CardTrick {
@@ -18,12 +14,61 @@ public class CardTrick {
     public static void main(String[] args)
     {
         Card[] magicHand = new Card[7];
-        
+        Scanner input = new Scanner(System.in);
         for (int i=0; i<magicHand.length; i++)
         {
+            //chooses random suit and value. Does not account for duplicates
+            int randomSuit = (int)(Math.random() * 4);
+            int randomValue = (int)((Math.random() * 13) + 1);
             Card c = new Card();
+            c.setSuit(c.SUITS[randomSuit]);
+            c.setValue(randomValue);
+            magicHand[i] = c;
              }
         
+        //initializing input values and counter.
+        String suit;
+        int val;
+        int i;
+        
+        //do while looping input until it gives a valid suit. Not case-sensitive.
+        do {
+            System.out.println("Pick a valid card suit (Hearts, Diamonds, Spades, Clubs)");
+            suit = input.nextLine();
+            for (i = 0; i < 3; i++) {
+                if (Card.SUITS[i].toLowerCase().equals(suit.toLowerCase())) {
+                    break;
+                }
+            }
+        }
+        while (!(Card.SUITS[i].toLowerCase().equals(suit.toLowerCase())));
+        
+        //do while looping input until it gives a valid value.
+        do {
+            System.out.println("Pick a value that corresponds to a rank (1-13, where 1 is Ace, 13 is King, etc.)");
+            val = input.nextInt();
+        }
+        while (!(val > 0 && val < 14));
+        
+        //Creates a Card object that has inputted values.
+        Card yourCard = new Card();
+        yourCard.setSuit(suit);
+        yourCard.setValue(val);
+        
+        //Compares yourCard and the array of Card objects magicHand.
+        int b = 0;
+        while (b < 7) {
+            if (magicHand[b].getSuit().toLowerCase().equals(yourCard.getSuit().toLowerCase())) {
+                if (magicHand[b].getValue()== yourCard.getValue()) {
+                    yourCard.printInfo();
+                    break;
+                }
+            }
+            b += 1;
+            if (b == 7) {
+                System.out.println("Your card is not in the magic hand...");
+            }
+        }
          }
     
 }
