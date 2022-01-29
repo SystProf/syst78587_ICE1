46a46a9ad2577f670991830678261f7bf5ec132c
mianmur <mianmur@sheridancollege.ca>
2022-01-28 21:59:33 -0500

    added name and date

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -4,7 +4,8 @@
  * and open the template in the editor.
  */
 package ca.sheridancollege.week3.softwarefundamentals.ice1;
-
+import java.util.Random; 
+import java.util.Scanner;
 /**
  * A class that fills a magic hand of 7 cards with random Card Objects
  * and then asks the user to pick a card and searches the array of cards
@@ -16,15 +17,43 @@ package ca.sheridancollege.week3.softwarefundamentals.ice1;
  */
 public class CardTrick {
     
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         Card[] magicHand = new Card[7];
+        Scanner input = new Scanner(System.in);
+        String suit = "";
         
         for (int i=0; i<magicHand.length; i++)
         {
-            Card c = new Card();
-             }
+            magicHand[i] = new Card();
+            magicHand[i].setSuit(Card.SUITS[(int) (Math.random() * 4)]);
+            magicHand[i].setValue((int)(Math.random() * (13 - 1 + 1) + 1));
+//            System.out.println(magicHand[i].getValue() + " " + magicHand[i].getSuit());
+//            commented out for debugging
+        }
         
-         }
+        System.out.print("Guess the SUIT: (1.Hearts 2.Diamonds 3.Spades 4.Clubs): ");
+        int suitGuess = input.nextInt();
+        System.out.print("Guess a CARD (1-13): ");
+        int value = input.nextInt();
+        input.close();
+        
+        switch(suitGuess) {
+            case 1: suit = "Hearts"; break;
+            case 2: suit = "Diamonds"; break;
+            case 3: suit = "Spades"; break;
+            case 4: suit = "Clubs"; break;
+        }
+        
+        boolean compare = false;
+        
+        for (int i=0; i<magicHand.length; i++) {
+            if (magicHand[i].getValue() == value && suit.equals(magicHand[i].getSuit())) {
+                compare = true;
+            }
+        }
+        
+        System.out.println("You guessed: " + (compare == true ? "correctly" : "wrongly"));
+        
+    }
     
 }
